aca198fa5e73b131412011094f12584046b38a7f
fooweiguo <fooweiguo@hotmail.com>
2023-10-21 11:44:09 +0800

    updated bucket

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "sctp-ce3-tfstate-bucket-0"
+    bucket = "sctp-ce3-tfstate-bucket-1"
     key    = "sctp-endless.tfstate" #Change the value  of this to yourname-docker-ec2.tfstate for  example
     region = "us-east-1"
   }
